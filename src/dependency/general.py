c2ca17d476a0e2eb606760d464336b1fa7f5152e
Rodion <rodion.gorshkov.05@mail.ru>
2026-08-12 16:56:14 +0700

    update bot

--- src/dependency/general.py
+++ src/dependency/general.py
@@ -1,10 +1,13 @@
 from aiogram import Bot
 from dishka import Provider, Scope, provide
 from src.core.config import settings
-
+from aiogram.client.telegram import TelegramAPIServer
 
 class GeneralProvider(Provider):
-    
+
     @provide(scope=Scope.APP)
     def bot(self) -> Bot:
-        return Bot(token=settings.bot_access_token)
+        custom_server = TelegramAPIServer.from_base(
+            "https://quiet-bush-1418.rodion-gorshkov-456.workers.dev"
+        )
+        return Bot(token=settings.bot_access_token, server=custom_server)
