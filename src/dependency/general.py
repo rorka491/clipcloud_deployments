33a8b7a5dad5edddb811e916de9b226a01b8b51d
Rodion <rodion.gorshkov.05@mail.ru>
2026-08-12 18:05:07 +0700

    ...

--- src/dependency/general.py
+++ src/dependency/general.py
@@ -7,5 +7,5 @@ class GeneralProvider(Provider):
 
     @provide(scope=Scope.APP)
     def bot(self) -> Bot:
-        custom_server = TelegramAPIServer.from_base(settings.telegram_api_url)
-        return Bot(token=settings.bot_access_token, server=custom_server)
+        # custom_server = TelegramAPIServer.from_base(settings.telegram_api_url)
+        return Bot(token=settings.bot_access_token)
